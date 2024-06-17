c360b2c2b7e86dcdbe415a031b8299ba8d9b275f
Dan Cassidy <5643061+rux616@users.noreply.github.com>
2024-06-17 12:40:37 +1200

    version update

--- .version.ps1
+++ .version.ps1
@@ -20,7 +20,7 @@
 
 # version info
 $version_major = 1
-$version_minor = 9
+$version_minor = 10
 $version_patch = 0
 
 # prerelease info
@@ -31,5 +31,5 @@ $is_beta = $false
 $is_alpha = $false
 
 # build info
-$build_number = 33
+$build_number = 34
 $include_build_in_version = $false
